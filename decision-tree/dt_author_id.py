47823638354b6028e9f1d8ccf433372a73be475a
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-09-06 16:58:27 +0300

    done dt

--- decision-tree/dt_author_id.py
+++ decision-tree/dt_author_id.py
@@ -1,31 +1,32 @@
-#!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
-import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+def dt_classifier():
+    """
+        This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+        Use a Decision Tree to identify emails from the Enron corpus by author:
+        Sara has label 0
+        Chris has label 1
+    """
+
+    # features_train and features_test are the features for the training
+    # and testing datasets, respectively
+    # labels_train and labels_test are the corresponding item labels
+    features_train, features_test, labels_train, labels_test = preprocess()
+
+    #########################################################
+    # your code goes here ###
+    clf = DecisionTreeClassifier(min_samples_split=40)
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+    return accuracy_score(labels_test, pred)
+    #########################################################
 
 
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
+if __name__ == '__main__':
+    print(dt_classifier())
 
