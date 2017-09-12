3092446cfae0800773f420a5f6876d6d5ed8b315
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-09-12 19:20:37 +0300

    done regression (1st part)

--- _notes/regression.md
+++ _notes/regression.md
@@ -6,3 +6,12 @@ DISCRETE - weather, phone, author of the email;
 
 - finally I understood that `slope` is just `a` coefficient
 and `b` is an `intercept` in `y = ax + b`;
+
+- we use R^2 metric (instead of accuracy_score) - `reg.score()`;
+SSE is not normalized (bigger number of point gives us bigger SSE);
+
+- how to visualize? (a) plot the line and the scatter plot of data; 
+(b) plot errors; visualization is useful to understand if it's 
+possible to get a good linear regression on given data;
+
+### mini-project
--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """
     Starter code for the regression mini-project.
     
@@ -13,58 +11,66 @@
 """    
 
 
-import sys
 import pickle
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
-
-### list the features you want to look at--first item in the 
-### list will be the "target" feature
-features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
-target, features = targetFeatureSplit( data )
-
-### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
-feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
-train_color = "b"
-test_color = "b"
-
-
-
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
-
-
-
-
-
-
-
-
-### draw the scatterplot, with color-coded training and testing points
+from tools.feature_format import featureFormat, targetFeatureSplit
 import matplotlib.pyplot as plt
-for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
-for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
-
-### labels for the legend
-plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
-plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
+def get_data():
+    dictionary = pickle.load(open("final_project_dataset_modified.pkl", "rb"))
+    features_list = ["bonus", "salary"]
+    # list the features you want to look at--first item in the
+    # list will be the "target" feature
+    data = featureFormat(dictionary, features_list, remove_any_zeroes=True,
+                         sort_keys='../tools/python2_lesson06_keys.pkl')
+    target, features = targetFeatureSplit(data)
+
+    return train_test_split(features, target, test_size=0.5, random_state=42)
 
 
-### draw the regression line, once it's coded
-try:
-    plt.plot( feature_test, reg.predict(feature_test) )
-except NameError:
-    pass
-plt.xlabel(features_list[1])
-plt.ylabel(features_list[0])
-plt.legend()
-plt.show()
+def fit_regression():
+    feature_train, feature_test, target_train, target_test = get_data()
+
+    # Your regression goes here!
+    # Please name it reg, so that the plotting code below picks it up and
+    # plots it correctly. Don't forget to change the test_color above from "b" to
+    # "r" to differentiate training points from test points.
+    reg = LinearRegression()
+    reg.fit(feature_train, target_train)
+
+    return reg
+
+
+def plot_data():
+    feature_train, feature_test, target_train, target_test = get_data()
+    features_list = ["bonus", "salary"]
+    reg = fit_regression()
+
+    # draw the scatterplot, with color-coded training and testing points
+    train_color = "b"
+    test_color = "r"
+
+    for feature, target in zip(feature_test, target_test):
+        plt.scatter(feature, target, color=test_color)
+    for feature, target in zip(feature_train, target_train):
+        plt.scatter(feature, target, color=train_color)
+
+    # labels for the legend
+    plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
+    plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+
+    # draw the regression line, once it's coded
+    try:
+        plt.plot(feature_test, reg.predict(feature_test))
+    except NameError:
+        pass
+    plt.xlabel(features_list[1])
+    plt.ylabel(features_list[0])
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_data()
