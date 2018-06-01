ba5f2cb5512893ca1650e174439e22855f5fca5d
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-06-01 16:01:55 +0300

    found first important word

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python
 
 import pickle
-import numpy
-numpy.random.seed(42)
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
+def get_data():
+    words_file = "word_data_overfit.pkl"
+    authors_file = "email_authors_overfit.pkl"
+    word_data = pickle.load(open(words_file, "rb"))
+    authors = pickle.load(open(authors_file, "rb"))
+
+    features_train, features_test, labels_train, labels_test = \
+        train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
+    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                                 stop_words='english')
+    features_train = vectorizer.fit_transform(features_train)
+    features_test = vectorizer.transform(features_test).toarray()
+
+    print(vectorizer.get_feature_names()[33604])
+
+    ### a classic way to overfit is to use a small number
+    ### of data points and a large number of features;
+    ### train on only 150 events to put ourselves in this regime
+    features_train = features_train[:150].toarray()
+    labels_train = labels_train[:150]
+    return features_train, features_test, labels_train, labels_test
 
 
-
-### test_size is the percentage of events assigned to the test set (the
-### remainder go into training)
-### feature matrices changed to dense representations for compatibility with
-### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                             stop_words='english')
-features_train = vectorizer.fit_transform(features_train)
-features_test  = vectorizer.transform(features_test).toarray()
+def fit_decision_tree():
+    dtc = DecisionTreeClassifier()
+    dtc.fit(features_train, labels_train)
+    labels_pred = dtc.predict(features_test)
+    return accuracy_score(labels_test, labels_pred), dtc.feature_importances_
 
 
-### a classic way to overfit is to use a small number
-### of data points and a large number of features;
-### train on only 150 events to put ourselves in this regime
-features_train = features_train[:150].toarray()
-labels_train   = labels_train[:150]
+def get_most_important_features():
+    return [(i, f) for i, f in enumerate(feature_importance) if f >= .2]
 
 
-
-### your code goes here
-
-
-
+if __name__ == '__main__':
+    np.random.seed(42)
+    features_train, features_test, labels_train, labels_test = get_data()
+    # accuracy, feature_importance = fit_decision_tree()
+    # print(get_most_important_features())
